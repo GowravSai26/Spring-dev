34b311c05af0f41183d3f945f09105e753416182
Gowrav Sai <gowravnagsai@gmail.com>
2025-03-20 23:01:56 +0530

    ToDo-Application

---
+++ TodoApp/src/main/java/com/app/TodoApp/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.app.TodoApp.repository;
+
+import com.app.TodoApp.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+
+
+}
